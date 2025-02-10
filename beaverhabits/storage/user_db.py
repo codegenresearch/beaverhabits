b00a10238c5bf445f66fb108ab342f6fbc99f3f8
venkatkrishna <venkatkrishna@itchy>
2025-02-10 11:15:45 +0100

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/user_db.py
+++ beaverhabits/storage/user_db.py
@@ -26,22 +26,22 @@ class DatabasePersistentDict(observables.ObservableDict):
 
 
 class UserDatabaseStorage(UserStorage[DictHabitList]):
-    async def get_user_habit_list(self, user: User) -> Optional[DictHabitList]:
+    async def fetch_user_habit_list(self, user: User) -> Optional[DictHabitList]:
         user_habit_list = await crud.get_user_habit_list(user)
         if user_habit_list is None:
             return None
 
-        d = DatabasePersistentDict(user, user_habit_list.data)
-        return DictHabitList(d)
+        persistent_data = DatabasePersistentDict(user, user_habit_list.data)
+        return DictHabitList(persistent_data)
 
-    async def save_user_habit_list(self, user: User, habit_list: DictHabitList) -> None:
+    async def update_user_habit_list(self, user: User, habit_list: DictHabitList) -> None:
         await crud.update_user_habit_list(user, habit_list.data)
 
-    async def merge_user_habit_list(
-        self, user: User, other: DictHabitList
-    ) -> DictHabitList:
-        current = await self.get_user_habit_list(user)
-        if current is None:
+    async def merge_user_habit_list(self, user: User, other: DictHabitList) -> DictHabitList:
+        current_habit_list = await self.fetch_user_habit_list(user)
+        if current_habit_list is None:
             return other
 
-        return await current.merge(other)
+        merged_habit_list = await current_habit_list.merge(other)
+        await self.update_user_habit_list(user, merged_habit_list)
+        return merged_habit_list
