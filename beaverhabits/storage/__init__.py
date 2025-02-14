aa030dbb06b3c40ea732a1f995bf70d476947c75
venkatkrishna <venkatkrishna@moe>
2025-02-14 12:33:48 +0100

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/__init__.py
+++ beaverhabits/storage/__init__.py
@@ -1,8 +1,8 @@
 from beaverhabits.configs import StorageType, settings
-from beaverhabits.storage.session_file import SessionDictStorage, SessionStorage
+from beaverhabits.storage.session_file import SessionDictStorage, KEY_NAME as SESSION_KEY_NAME
 from beaverhabits.storage.storage import UserStorage
 from beaverhabits.storage.user_db import UserDatabaseStorage
-from beaverhabits.storage.user_file import UserDiskStorage
+from beaverhabits.storage.user_file import UserDiskStorage, KEY_NAME as USER_KEY_NAME
 
 session_storage = SessionDictStorage()
 user_disk_storage = UserDiskStorage()
@@ -10,15 +10,14 @@ user_database_storage = UserDatabaseStorage()
 sqlite_storage = None
 
 
-def get_sessions_storage() -> SessionStorage:
+def get_sessions_storage() -> SessionDictStorage:
     return session_storage
 
 
-def get_user_dict_storage() -> UserStorage:
+def get_user_storage() -> UserStorage:
     if settings.HABITS_STORAGE == StorageType.USER_DISK:
         return user_disk_storage
-
-    if settings.HABITS_STORAGE == StorageType.USER_DATABASE:
+    elif settings.HABITS_STORAGE == StorageType.USER_DATABASE:
         return user_database_storage
-
-    raise NotImplementedError("Storage type not implemented")
+    else:
+        raise NotImplementedError("Storage type not implemented")
